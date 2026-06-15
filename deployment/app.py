1796ff095e1196f63d9a995368707230736526cd
sitimulyani23 <159735483+sitimulyani23@users.noreply.github.com>
2026-06-15 18:46:40 +0700

    Update app.py

--- deployment/app.py
+++ deployment/app.py
@@ -1,29 +1,31 @@
-import streamlit as st
+import gradio as gr
 import pickle
 
 # Load model dan TF-IDF
 model = pickle.load(open("model_logreg.pkl", "rb"))
 tfidf = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
 
-st.title("Analisis Sentimen Spotify Indonesia")
+# Fungsi prediksi
+def predict_sentiment(review):
 
-st.write("Masukkan ulasan Spotify untuk diprediksi sentimennya")
+    review_tfidf = tfidf.transform([review])
 
-review = st.text_area("Masukkan Ulasan")
+    prediction = model.predict(review_tfidf)[0]
 
-if st.button("Prediksi"):
+    return prediction
 
-    if review.strip() != "":
 
-        review_tfidf = tfidf.transform([review])
+# Interface Gradio
+demo = gr.Interface(
+    fn=predict_sentiment,
+    inputs=gr.Textbox(
+        lines=5,
+        placeholder="Masukkan ulasan Spotify...",
+        label="Ulasan"
+    ),
+    outputs=gr.Textbox(label="Hasil Prediksi"),
+    title="Analisis Sentimen Spotify Indonesia",
+    description="Prediksi sentimen ulasan Spotify menggunakan Logistic Regression."
+)
 
-        prediction = model.predict(review_tfidf)[0]
-
-        if prediction == "positif":
-            st.success("Sentimen Positif 😊")
-
-        elif prediction == "negatif":
-            st.error("Sentimen Negatif 😠")
-
-        else:
-            st.warning("Sentimen Netral 😐")
+demo.launch()
